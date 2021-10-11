2789d80ed22e26a1c7f8f8e4fed334cc061b1938
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-10-11 14:05:43 -0400

    Test Strassen matrix multiplication concatenation

--- TP1/tp1/src/main.rs
+++ TP1/tp1/src/main.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-use ndarray::{arr2, Array2, s};
+use ndarray::{arr2, Array2, s, concatenate, Axis, stack};
 
 fn multiply_matrices_conventional(matrix_1: &Array2<u32>, matrix_2: &Array2<u32>) -> Array2<u32> {
     let n = matrix_1.shape()[0];
@@ -38,10 +38,27 @@ fn multiply_matrices_strassen(matrix_1: &Array2<u32>, matrix_2: &Array2<u32>) ->
         ];
 
         let intermediate_matrices = [ // M
-            multiply_matrices_strassen(&(&matrix_1_slices[0] + &matrix_1_slices[3]), &(&matrix_2_slices[0] + &matrix_2_slices[3])),
-            // TODO
+            multiply_matrices_strassen(&(&matrix_1_slices[0] + &matrix_1_slices[3]), &(&matrix_2_slices[0] + &matrix_2_slices[3])), // M1 = (A1,1 + A2,2) * (B1,1 + B2,2)
+            multiply_matrices_strassen(&(&matrix_1_slices[2] + &matrix_1_slices[3]), &matrix_2_slices[0].to_owned()), // M2 = (A2,1 + A2,2) * B1,1
+            multiply_matrices_strassen(&matrix_1_slices[0].to_owned(), &(&matrix_2_slices[1] - &matrix_2_slices[3])), // M3 = A1,1 * (B1,2 - B2,2)
+            multiply_matrices_strassen(&matrix_1_slices[3].to_owned(), &(&matrix_2_slices[2] - &matrix_2_slices[0])), // M4 = A2,2 * (B2,1 - B1,1)
+            multiply_matrices_strassen(&(&matrix_1_slices[0] + &matrix_1_slices[1]), &matrix_2_slices[3].to_owned()), // M5 = (A1,1 + A1,2) * B2,2
+            multiply_matrices_strassen(&(&matrix_1_slices[2] - &matrix_1_slices[0]), &(&matrix_2_slices[0] + &matrix_2_slices[1])), // M6 = (A2,1 - A1,1) * (B1,1 + B1,2)
+            multiply_matrices_strassen(&(&matrix_1_slices[1] - &matrix_1_slices[3]), &(&matrix_2_slices[2] + &matrix_2_slices[3])), // M7 = (A1,2 - A2,2) * (B2,1 + B2,2)
         ];
-        println!("{}", intermediate_matrices[0]);
+        
+        let result_quadrants = [ // C
+            &intermediate_matrices[0] + &intermediate_matrices[3] - &intermediate_matrices[4] + &intermediate_matrices[6], // C1,1 = M1 + M4 - M5 + M7
+            &intermediate_matrices[2] + &intermediate_matrices[4], // C1,2 = M3 + M5
+            &intermediate_matrices[1] + &intermediate_matrices[3], // C2,1 = M2 + M4
+            &intermediate_matrices[0] - &intermediate_matrices[1] + &intermediate_matrices[2] + &intermediate_matrices[5], // C2,2 = M1 - M2 + M3 + M6
+        ];
+        let con = concatenate![Axis(1), result_quadrants[0], result_quadrants[1]];
+        let sta = stack![Axis(1), result_quadrants[0], result_quadrants[1]];
+        println!("result_quadrants[0]: {}", result_quadrants[0]);
+        println!("result_quadrants[1]: {}", result_quadrants[1]);
+        println!("con: {}", con);
+        println!("sta: {}", sta);
     }
 
     result
