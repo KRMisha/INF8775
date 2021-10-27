f8a14c9dac2dee51f8c46d36c807600d44173f31
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-10-27 13:36:56 -0400

    Add argument parsing boilerplate

---
+++ TP2/implementation/src/cli_args.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub enum Algorithm {
+    Greedy,
+    BranchBound,
+    Taboo,
+}
+
+impl FromStr for Algorithm {
+    type Err = String;
+    fn from_str(algorithm: &str) -> Result<Self, Self::Err> {
+        match algorithm {
+            "glouton" => Ok(Algorithm::Greedy),
+            "branch_bound" => Ok(Algorithm::BranchBound),
+            "tabou" => Ok(Algorithm::Taboo),
+            _ => Err(format!("Could not parse algorithm: {}", algorithm)),
+        }
+    }
+}
+
+#[derive(StructOpt)]
+pub struct Cli {
+    /// The multiplication algorithm to use
+    #[structopt(short)]
+    pub algorithm: Algorithm,
+
+    /// Path to the graph adjacency matrix file
+    #[structopt(short = "e", parse(from_os_str))]
+    pub filename: PathBuf,
+
+    /// Prints the solution
+    #[structopt(short = "p")]
+    pub show_result: bool,
+
+    /// Prints execution time in ns
+    #[structopt(short = "t")]
+    pub show_exec_time: bool,
+}
--- TP2/implementation/src/main.rs
+++ TP2/implementation/src/main.rs
@@ -1,3 +1,51 @@
+use std::time::Instant;
+
+use ndarray::{Array2};
+use structopt::StructOpt;
+
+mod cli_args;
+mod matrix_utils;
+
+use cli_args::{Algorithm, Cli};
+use matrix_utils::{load_matrix};
+
 fn main() {
-    println!("Hello, world!");
+    // Parse args
+    let args = Cli::from_args();
+
+    // Load matrices
+    let graph_matrix = load_matrix(&args.filename).expect("Error parsing graph adjacency matrix from file");
+
+    // Start clock
+    let now = Instant::now();
+
+    // Execute selected algorithm
+    let result = match args.algorithm {
+        Algorithm::Greedy => solve_with_greedy(&graph_matrix),
+        Algorithm::BranchBound => solve_with_branch_bound(&graph_matrix),
+        Algorithm::Taboo => solve_with_taboo(&graph_matrix),
+    };
+
+    // Calculate elapsed time
+    let elapsed_ms = now.elapsed().as_secs_f64() * 1000.0;
+
+    if args.show_result {
+        // TODO
+    }
+
+    if args.show_exec_time {
+        println!("{}", elapsed_ms);
+    }
+}
+
+fn solve_with_greedy(graph_matrix: &Array2<u8>) {
+
+}
+
+fn solve_with_branch_bound(graph_matrix: &Array2<u8>) {
+    
+}
+
+fn solve_with_taboo(graph_matrix: &Array2<u8>) {
+    
 }
