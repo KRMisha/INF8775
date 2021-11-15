a22d88a8891139424a9792ea111fd03a61fe62d1
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-11-15 17:15:54 -0500

    Update list of generated instance sizes

--- TP2/generate_instances.sh
+++ TP2/generate_instances.sh
@@ -5,8 +5,8 @@ readonly OUTPUT_DIRECTORY=data/generated
 
 mkdir -p ${OUTPUT_DIRECTORY}
 
-for i in {8..64..8}; do
-    for j in {0..4}; do
+for i in 8 16 24 32 40 48 56 64 72 80; do
+    for j in {0..9}; do
         filename=ex${i}_${j}
         ./data/generator/bin/generator "${OUTPUT_DIRECTORY}/${filename}" \
             <<< "0 ${SEED}${j} 1 $i 1 0.9" > /dev/null
