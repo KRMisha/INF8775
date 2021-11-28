393b83d319c91f94e6a74d7733086aca75e92d9a
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-11-28 16:13:43 -0500

    Add placeholder algorithm returning vector of node indices

--- TP3/implementation/src/algorithm.rs
+++ TP3/implementation/src/algorithm.rs
@@ -1,5 +1,15 @@
-use petgraph::graph::UnGraph;
+use petgraph::graph::{NodeIndex, UnGraph};
+use petgraph::visit::Dfs;
 
-pub fn solve(graph: &UnGraph<u16, ()>) {
-    // TODO
+pub fn solve(graph: &UnGraph<u16, ()>) -> Vec<NodeIndex> {
+    // TODO: Replace placeholder algorithm
+
+    let mut dfs = Dfs::new(graph, 0.into());
+
+    let mut sorted_node_indices = Vec::with_capacity(graph.node_count());
+    while let Some(node_index) = dfs.next(graph) {
+        sorted_node_indices.push(node_index);
+    }
+
+    sorted_node_indices
 }
