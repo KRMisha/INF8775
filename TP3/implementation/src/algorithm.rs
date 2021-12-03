1f96b98bdbf25b57fd7e4fc95a72c2a07db5dc91
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-12-03 16:19:42 -0500

    Implement backtracking Hamiltonian path finder algorithm

--- TP3/implementation/src/algorithm.rs
+++ TP3/implementation/src/algorithm.rs
@@ -1,20 +1,71 @@
+use std::collections::HashSet;
+
 use petgraph::graph::{NodeIndex, NodeReferences, UnGraph};
-use petgraph::visit::{Dfs, IntoNodeReferences};
+use petgraph::visit::IntoNodeIdentifiers;
 
 pub fn solve(graph: &UnGraph<u16, ()>) -> Vec<NodeIndex> {
-    // TODO: Replace placeholder algorithm
-
-    let starting_node_index = find_node_with_minimum_weight(graph.node_references());
-    let mut dfs = Dfs::new(graph, starting_node_index);
-
-    let mut sorted_node_indices = Vec::with_capacity(graph.node_count());
-    while let Some(node_index) = dfs.next(graph) {
-        sorted_node_indices.push(node_index);
+    let hamiltonian_path = find_hamiltonian_path_with_backtracking(graph);
+    match hamiltonian_path {
+        Some(hamiltonian_path) => hamiltonian_path,
+        None => {
+            // TODO: Replace with Option return type
+            // or avoid this problem by integrating the multiple solution loop here
+            // or by making the solve function include the backtracking algorithm
+            eprintln!("Error: No path found");
+            Vec::new()
+        }
     }
-
-    sorted_node_indices
 }
 
+#[allow(dead_code)]
 fn find_node_with_minimum_weight(nodes: NodeReferences<u16>) -> NodeIndex {
     nodes.min_by_key(|&(_, weight)| weight).unwrap().0.into()
+}
+
+fn find_hamiltonian_path_with_backtracking(graph: &UnGraph<u16, ()>) -> Option<Vec<NodeIndex>> {
+    // Stack of partial paths to visit
+    let mut paths_to_visit = Vec::new();
+
+    // Any node can act as the starting node for the search tree
+    let ordered_starting_nodes = get_ordered_starting_nodes(graph);
+    paths_to_visit.extend(ordered_starting_nodes.into_iter().rev().map(|n| vec![n]));
+
+    while let Some(current_path) = paths_to_visit.pop() {
+        let is_path_complete = current_path.len() == graph.node_count();
+        if is_path_complete {
+            // TODO: Yield multiple paths rather than returning on first solution (iterate to improve solution)
+            return Some(current_path);
+        }
+
+        // Add extended paths in reverse order to pop and visit most promising paths first
+        let extended_paths = extend_path(graph, &current_path);
+        paths_to_visit.extend(extended_paths.into_iter().rev());
+    }
+
+    None
+}
+
+fn get_ordered_starting_nodes(graph: &UnGraph<u16, ()>) -> Vec<NodeIndex> {
+    // TODO: Select nodes in order of estimated relevance (heuristic)
+    graph.node_identifiers().collect()
+}
+
+fn extend_path(graph: &UnGraph<u16, ()>, path: &[NodeIndex]) -> Vec<Vec<NodeIndex>> {
+    let path_node_set: HashSet<NodeIndex> = path.iter().cloned().collect();
+
+    let mut extended_paths = Vec::new();
+
+    // TODO: Consider extending path at either front or back of vector
+    if let Some(&last_node_index) = path.last() {
+        let unvisited_neighbor_node_indices: Vec<_> = graph
+            .neighbors(last_node_index)
+            .filter(|x| !path_node_set.contains(x))
+            .collect();
+        // TODO: Add extended paths in order of estimated relevance (heuristic)
+        for neighbor_node_index in unvisited_neighbor_node_indices {
+            extended_paths.push([path, &[neighbor_node_index]].concat());
+        }
+    }
+
+    extended_paths
 }
