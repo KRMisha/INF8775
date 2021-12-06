fbcb0c71bd0dc3d4ec48fd98c8b90520d653f872
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-12-06 17:23:13 -0500

    Precompute node neighbor ordering to speed up path extension

--- TP3/implementation/src/algorithm.rs
+++ TP3/implementation/src/algorithm.rs
@@ -4,7 +4,7 @@ use std::iter;
 use itertools::Itertools;
 use petgraph::graph::{NodeIndex, NodeReferences, UnGraph};
 use petgraph::visit::IntoNodeIdentifiers;
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 use crate::utils::{count_obstructions, print_solution};
 
@@ -12,13 +12,12 @@ use crate::utils::{count_obstructions, print_solution};
 // - If using bidirectional path extension idea, extend with lighter nodes on the left, heavier on the right
 // - Use median weight (or close to) for start node
 // - Use sort based on a fuzzy mix between node weights and degrees (Euclidian distance to (0, 0) of degree-weight tuples?)
-// - Use Held-Karp table to speed up calculations of branch leaves when remaining nodes is below threshold (e.g. 5)
-//   This would allow quickly checking if a path exists, and cutting off iteration in those search paths earlier
-// - Use FxHashSet in extend_path() when the number of neighbors is greater than a certain threshold
+// - Use FxHashSet in extend_path() when the number of neighbors is greater than a certain threshold (tinyset)
 
 pub fn solve_in_loop(graph: &UnGraph<u16, ()>, should_display_full_solution: bool) {
-    // Precompute node degrees
+    // Precompute node degrees and ordered node neighbors
     let node_degrees = get_node_degrees(graph);
+    let ordered_node_neighbors = get_ordered_node_neighbors(graph, &node_degrees);
 
     // Stack of partial paths to visit
     let mut paths_to_visit = Vec::new();
@@ -51,7 +50,12 @@ pub fn solve_in_loop(graph: &UnGraph<u16, ()>, should_display_full_solution: boo
         }
 
         // Add extended paths in reverse order to pop and visit most promising paths first
-        let extended_paths = extend_path(graph, &node_degrees, &current_path, &current_path_set);
+        let extended_paths = extend_path(
+            graph,
+            &ordered_node_neighbors,
+            &current_path,
+            &current_path_set,
+        );
         paths_to_visit.extend(extended_paths.into_iter().rev());
     }
 }
@@ -80,9 +84,28 @@ fn get_ordered_starting_nodes(graph: &UnGraph<u16, ()>) -> Vec<NodeIndex> {
     node_indices
 }
 
-fn extend_path(
+fn get_ordered_node_neighbors(
     graph: &UnGraph<u16, ()>,
     node_degrees: &HashMap<NodeIndex, u32>,
+) -> FxHashMap<NodeIndex, Vec<NodeIndex>> {
+    let mut ordered_node_neighbors = FxHashMap::default();
+
+    // TODO: Add extended paths in order of estimated relevance (heuristic)
+    for node_index in graph.node_identifiers() {
+        let node_neighbors: Vec<_> = graph
+            .neighbors(node_index)
+            .sorted_unstable_by_key(|n| node_degrees.get(n))
+            .collect();
+
+        ordered_node_neighbors.insert(node_index, node_neighbors);
+    }
+
+    ordered_node_neighbors
+}
+
+fn extend_path(
+    graph: &UnGraph<u16, ()>,
+    ordered_node_neighbors: &FxHashMap<NodeIndex, Vec<NodeIndex>>,
     path: &[NodeIndex],
     path_set: &FxHashSet<NodeIndex>,
 ) -> Vec<(Vec<NodeIndex>, FxHashSet<NodeIndex>)> {
@@ -91,15 +114,14 @@ fn extend_path(
     let unvisited_node_count = graph.node_count() - path.len();
 
     // Extend path at back of vector
-    if let Some(&last_node_index) = path.last() {
-        let unvisited_neighbor_node_indices: Vec<_> = graph
-            .neighbors(last_node_index)
-            .filter(|x| !path_set.contains(x))
-            .sorted_unstable_by_key(|n| node_degrees.get(n))
-            .collect();
+    if let Some(last_node_index) = path.last() {
+        let unvisited_neighbor_node_indices = ordered_node_neighbors
+            .get(last_node_index)
+            .unwrap()
+            .iter()
+            .filter(|x| !path_set.contains(x));
 
-        // TODO: Add extended paths in order of estimated relevance (heuristic)
-        for neighbor_node_index in unvisited_neighbor_node_indices {
+        for &neighbor_node_index in unvisited_neighbor_node_indices {
             // Unreacheable node heuristic
             let mut has_unreachable_second_neighbor = false;
             if unvisited_node_count > 2 {
@@ -140,15 +162,14 @@ fn extend_path(
         return extended_paths;
     }
 
-    if let Some(&first_node_index) = path.first() {
-        let unvisited_neighbor_node_indices: Vec<_> = graph
-            .neighbors(first_node_index)
-            .filter(|x| !path_set.contains(x))
-            .sorted_unstable_by_key(|n| node_degrees.get(n))
-            .collect();
+    if let Some(first_node_index) = path.first() {
+        let unvisited_neighbor_node_indices = ordered_node_neighbors
+            .get(first_node_index)
+            .unwrap()
+            .iter()
+            .filter(|x| !path_set.contains(x));
 
-        // TODO: Add extended paths in order of estimated relevance (heuristic)
-        for neighbor_node_index in unvisited_neighbor_node_indices {
+        for &neighbor_node_index in unvisited_neighbor_node_indices {
             // Unreacheable node heuristic
             let mut has_unreachable_second_neighbor = false;
             if unvisited_node_count > 2 {
